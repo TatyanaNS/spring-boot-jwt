b6eb0c3cb3ae17799081c72f749da43fb6b5b63a
Tanya Novikova <39490267+TatyanaNS@users.noreply.github.com>
2022-07-04 10:12:15 +0300

    Delete target directory
    jwt realization

--- target/classes/db.migration/V1.0.0__Initial_migration_file.sql
+++
@@ -1,8 +0,0 @@
---users dictionary
-create table users(
-     id 	  serial      not null,
-     username varchar(50) not null,
-     email	  varchar(50) not null,
-     password varchar(100) not null,
-     CONSTRAINT users_pk PRIMARY KEY (id)
-);
